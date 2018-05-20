9e5725338856927d148c682e37786d716e69babd
Geloso Federico <gelosofederico@gmail.com>
2018-05-20 18:45:51 -0300

    Valores finales (dan hasta 2dB)

--- Diseno_multibanda_ventana.m
+++ Diseno_multibanda_ventana.m
@@ -3,13 +3,13 @@ start %corre el script que carga las cosas
 
 %% Parámetros más modificables
 % Ws son 0.01953 0.06452 0.1016 0.1452 10.1719 0.2227 0.4375 0.4805 0.707 0.9512
-Wc = [0.0365*pi 0.1284*pi 0.1872*pi 0.459*pi 0.8690*pi];  %Salen de promediar los omegas de a 2, pero hay que ir retocándolos para que funcione bien
-Amplitudes = [2.3045 0.2845 3.7990 0.5713 1.2910 3.5650];  %Salen de ver el gráfico de la inversa de H. También hay que retocarlos
+Wc = [0.0365*pi 0.133*pi 0.1872*pi 0.459*pi 0.78*pi 0.86*pi];  %Salen de promediar los omegas de a 2, pero hay que ir retocándolos para que funcione bien
+Amplitudes = [2.3045 0.2855 3.790 0.5713 1.3110 2.250 2.7];  %Salen de ver el gráfico de la inversa de H. También hay que retocarlos
 min_DW = 0.1*pi;                                           %Salen del mínimo deltaW para las bandas de transición
 %M = ceil( 8*pi/min_DW );                                %Usamos la ventana de Hann porque es la primera que llega al delta minimo tras corregir (0.0043/0.57)
-M=138;
+M=142;
 %% Diseña
-h_disenado = multibanda(Amplitudes,Wc./(2*pi),M,@hann);
+h_disenado = multibanda(Amplitudes,Wc./(2*pi),M,@hamming);
 
 %% Transformada y error
 nfft = 1024;            %Este número va a ser la definición de la fft. Usamos 1024 para todos
